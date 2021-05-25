22b0f7ff7d884558162e1ce4385a4aa88369be79
binshadcs <binshadcs@gmail.com>
2021-05-25 15:05:40 +0530

    last commit before deploye

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn eventManagement.wsgi --log-file -
--- eventManagement/settings.py
+++ eventManagement/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'x)wkf1)av_pp@5zaympxo9@0hh0xcs#!m)p5@mgy*4zi&h6gt!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://eventgateway.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
